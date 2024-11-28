61b7e7900730078c540451fffc681578e55b55ab
Nilton OS <jniltinho@gmail.com>
2024-11-28 14:23:36 -0300

    Add viper Config

--- env_loader.go
+++ env_loader.go
@@ -1,64 +1,84 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"os"
 
-	"gopkg.in/yaml.v3"
+	"github.com/spf13/viper"
 )
 
-type ENVRoot struct {
-	ENV ENV `yaml:"env"`
-}
-
 type ENV struct {
-	Host       string `yaml:"host"`
-	User       string `yaml:"user"`
-	PrivateKey string `yaml:"privateKey"`
-	Password   string `yaml:"password"`
-	Port       uint   `yaml:"port"`
-	Silent     bool   `yaml:"silent"`
+	Host       string `mapstructure:"ENV_HOST"`
+	User       string `mapstructure:"ENV_USER"`
+	PrivateKey string `mapstructure:"ENV_PRIVATEKEY"`
+	Password   string `mapstructure:"ENV_PASSWORD"`
+	Port       uint   `mapstructure:"ENV_PORT"`
+	Silent     bool   `mapstructure:"ENV_SILENT"`
 }
 
-var env ENVRoot
+var config ENV
 
 func loadENVs() {
 
-	yamlData, err := os.ReadFile(cfgFile)
+	initConfig()
 
-	if err != nil {
-		log.Fatalf("Failed to read config file: %v", err)
+	if config.Host != "" && os.Getenv("SSH_HOST") == "" {
+		os.Setenv("SSH_HOST", config.Host)
 	}
 
-	laodENVConfig(yamlData)
-
-	if env.ENV.Host != "" && os.Getenv("SSH_HOST") == "" {
-		os.Setenv("SSH_HOST", env.ENV.Host)
+	if config.User != "" && os.Getenv("SSH_USER") == "" {
+		os.Setenv("SSH_USER", config.User)
 	}
 
-	if env.ENV.User != "" && os.Getenv("SSH_USER") == "" {
-		os.Setenv("SSH_USER", env.ENV.User)
+	if config.PrivateKey != "" && os.Getenv("SSH_KEY_PATH") == "" {
+		os.Setenv("SSH_KEY_PATH", config.PrivateKey)
 	}
 
-	if env.ENV.PrivateKey != "" && os.Getenv("SSH_KEY_PATH") == "" {
-		os.Setenv("SSH_KEY_PATH", env.ENV.PrivateKey)
+	if config.Password != "" && os.Getenv("SSH_PASSWORD") == "" {
+		os.Setenv("SSH_PASSWORD", config.Password)
 	}
 
-	if env.ENV.Password != "" && os.Getenv("SSH_PASSWORD") == "" {
-		os.Setenv("SSH_PASSWORD", env.ENV.Password)
+	if config.Silent {
+		SetSilent = config.Silent
 	}
 
-	if env.ENV.Silent {
-		SetSilent = env.ENV.Silent
+	if config.Port != 0 {
+		SetPort = config.Port
 	}
 
-	if env.ENV.Port != 0 {
-		SetPort = env.ENV.Port
-	}
+	//fmt.Println("Server Address:", config.Host)
+	//fmt.Println("User:", config.User)
+	//fmt.Println("Key:", config.PrivateKey)
+	//fmt.Println("Silent:", config.Silent)
 
 }
 
-func laodENVConfig(yamlData []byte) {
-	yaml.Unmarshal(yamlData, &env)
-	//fmt.Println("Loaded ENV Config:", env)
+func initConfig() {
+
+	// 2. Read config file
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		viper.SetConfigName("config") // name of config file (without extension)
+		viper.AddConfigPath(".")      // optionally look for config in the working directory
+	}
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			// Config file not found; ignore error if desired
+		} else {
+			fmt.Println("Error reading config file:", err)
+			os.Exit(1)
+		}
+	}
+
+	// 3. Bind environment variables
+	viper.SetEnvPrefix("SSH") // Set environment variable prefix
+	viper.AutomaticEnv()      // Read in environment variables that match
+
+	// 4. Unmarshal config values
+	if err := viper.Unmarshal(&config); err != nil {
+		fmt.Println("Error unmarshalling config:", err)
+		os.Exit(1)
+	}
 }
